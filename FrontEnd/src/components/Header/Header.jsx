3a6a35fa966c9fb17877c218eb734289c2920f45
Tom <Tissot.tom31@gmail.com>
2023-10-17 10:21:46 +0200

    Set up provider ,use of the blind in the required locations

--- FrontEnd/src/components/Header/Header.jsx
+++ FrontEnd/src/components/Header/Header.jsx
@@ -1,25 +1,46 @@
-import { Link } from 'react-router-dom';
-import logo from '../../assets/img/argentBankLogo.webp';
+import { Link } from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux"
+import { setLogOut } from "../../redux/reducers/userAuthSlice"
+import logo from "../../assets/img/argentBankLogo.webp"
 
 export default function Header() {
-	return (
-		<header>
-			<nav className="main-nav">
-				<Link className="main-nav-logo" to="./">
-					<img
-						className="main-nav-logo-image"
-						src={logo}
-						alt="Argent Bank Logo"
-					/>
-					<h1 className="sr-only">Argent Bank</h1>
-				</Link>
-				<div>
-					<Link className="main-nav-item" to="./sign-in/">
-						<i className="fa fa-user-circle"></i>
-						&nbsp;Sign In
-					</Link>
-				</div>
-			</nav>
-		</header>
-	);
+    const token = useSelector(state => state.userAuth.token)
+    const profile = useSelector((state) => state.profile)
+    const dispatch = useDispatch()
+
+    return (
+        <header>
+            <nav className="main-nav">
+                <Link
+                    className="main-nav-logo"
+                    to="./" >
+                    <img
+                        className="main-nav-logo-image"
+                        src={logo}
+                        alt="Argent Bank Logo" />
+                    <h1 className="sr-only">Argent Bank</h1>
+                </Link>
+                <div>
+                    {token && (
+                        <Link
+                            className="main-nav-item"
+                            to="./user">
+                            {profile.userName}
+                        </Link>
+                    )}
+                    <Link
+                        className="main-nav-item"
+                        to={token ? "./" : "./sign-in/"}
+                        onClick={() => {
+                            if (token) {
+                                dispatch(setLogOut({}))
+                            }
+                        }}>
+                        <i className="fa fa-user-circle"></i>
+                        {token ? " Sign Out" : " Sign In"}
+                    </Link>
+                </div>
+            </nav>
+        </header>
+    )
 }
