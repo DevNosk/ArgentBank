6bec87cc42ce752737a32799e244a2fa3a0feefa
Tom <Tissot.tom31@gmail.com>
2023-11-09 11:45:35 +0100

    correction Banner

--- FrontEnd/src/components/Banner/Banner.jsx
+++ FrontEnd/src/components/Banner/Banner.jsx
@@ -1,7 +1,7 @@
 export default function Banner() {
 	return (
-		<div className="banner">
-			<section className="banner-content">
+		<div className="hero">
+			<section className="hero-content">
 				<h2 className="sr-only">Promoted Content</h2>
 				<p className="subtitle">No fees.</p>
 				<p className="subtitle">No minimum deposit.</p>
